df0a8356251f3cfdf29ad2d1af3f945c36d1575d
Edna-codes <guantaiedna2@gmail.com>
2024-05-27 21:24:29 +0300

    Routing for each page done

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,25 @@ import './App.css'
 import './index.css'
 import './components/Navbar'
 import Navbar from './components/Navbar'
+import { Routes, Route } from "react-router-dom";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import Services from "./components/pages/Services";
 
 function App() {
 
 
   return (
     <>
-      <Navbar/>
+      <Routes>
+        
+      <Route path='/' element={<Navbar/>} />
+      <Route path='/About' element={<About />}/>
+      <Route path='/Contact' element={<Contact />}/>
+      <Route path='/Services' element={<Services />}/>
+
+        
+      </Routes>
     </>
   )
 }
---
+++ src/components/pages/About.jsx
@@ -0,0 +1,17 @@
+import Navbar from "../Navbar"
+
+export default function About() {
+  return (
+     <>  
+     <Navbar/>
+     
+     <div>
+        <h1>About us</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque unde eum tempore voluptates error maxime assumenda dolore, nemo temporibus dolorem velit hic incidunt perferendis tempora quod sint esse quas perspiciatis? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod ab tempora corporis tempore officia necessitatibus nostrum voluptatum saepe doloremque, debitis esse magni autem voluptas nemo sit possimus dolore! Molestias, ipsa.</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum pariatur minus dicta ducimus quod repellendus consectetur eveniet voluptates aliquam, nostrum vero maiores accusamus sed tempora soluta quibusdam hic fuga harum dolorem accusantium, quia corporis? Consequatur suscipit repudiandae minima? Similique commodi illo, sed odio enim id laborum cum iure temporibus vel. Assumenda deleniti dolorum asperiores neque ipsam sint impedit quis alias non accusantium praesentium, cum mollitia est voluptates magnam! Consectetur recusandae adipisci, nobis sed rem accusamus architecto nam expedita cum magni debitis vel totam repellendus officiis, esse reprehenderit odio qui dolores non tempore ratione quaerat. Praesentium quo vel voluptas maiores voluptatum.</p>
+    </div>
+     </>
+ 
+
+  )
+}
