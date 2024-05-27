4ef3710c327afcdff8d2c8d971c62c3a86e6d1d3
Edna-codes <guantaiedna2@gmail.com>
2024-05-27 19:41:40 +0300

    Add logo and navlinks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,23 @@ import '../App.css'
 function Navbar () {
 
     return (
-        <div className='header'>Hello world!</div>
+        <>
+      <header className='nav-container'>
+      <nav className='navigation-bar'>
+            <div className='brand-logo'>
+                <h1>VROOM</h1>
+            </div>
+            <div className='nav-menu'>
+                <ul className='nav-list'>
+                    <li className='nav-item'>About</li>
+                    <li className='nav-item'>Contact</li>
+                    <li className='nav-item'>FAQs</li>
+                </ul>
+            </div>
+        </nav>
+      </header>
+        
+        </>
     )
 
 }
