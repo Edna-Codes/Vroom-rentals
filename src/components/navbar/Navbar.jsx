241181827daeb3fd27134beb19845c6c919391f7
Edna-codes <guantaiedna2@gmail.com>
2024-06-11 16:27:45 +0300

    Style navbar links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,65 +1,28 @@
-import './Navbar.css'
+import "./Navbar.css";
 import { Link } from "react-router-dom";
-import { navData } from '../../data'
+import { navData } from "../../data";
 
+function Navbar() {
+  return (
+    <>
+      <nav>
+        <Link className="tittle" to="./">
+          VROOM
+        </Link>
 
-
-
-
-function Navbar () {
-
-  
-
-    return (
-
-
-        <>
-           
-          <nav>
-            <Link className='tittle' to='./'>
-                VROOM
-            </Link>
-                  
-                    
-                    <div className='nav-menu'>
-                        <ul className='nav-list'>
-                       
+        <div className="nav-menu">
+          <ul className="nav-list">
             {navData.map((item, i) => (
-                <li key={i} className='nav-item'>
+              <li key={i} className="nav-item">
                 <Link to={item.href}>{item.title}</Link>
-                </li>
-
+              </li>
+            
             ))}
-              
-                            
-                        </ul>
-                    </div>
-                
-          </nav>
-              
-            </>
-      
-    //   <nav >
-    //         {/* <Link className='tittle' to="/">{NavData.title}</Link>
-        
-    //         <div className='nav-menu'>
-    //             <ul className='nav-list'>
-    //                 <li className='nav-item'>
-    //                     <Link to="/about">About</Link>
-    //                 </li>
-    //                 <li className='nav-item'> 
-    //                 <Link to="/contact">Contact</Link>
-    //                 </li>
-    //                 <li className='nav-item'>
-    //                     <Link to="/services">Services</Link>
-    //                 </li> 
-    //             </ul>
-    //         </div> */}
-    //     </nav>
-    
-    
-    )
-
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
